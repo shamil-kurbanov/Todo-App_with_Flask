a13ce9c659cc6193d8d76c989abf77d6bfd93b07
Shamil Kurbanov <kurbanov@outlook.de>
2024-02-17 14:19:40 +0100

    base.html, main.css, app.py were updated

--- ToDo-List/app.py
+++ ToDo-List/app.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+# Create Flask application instance
 app = Flask(__name__)
+# Configure SQLAlchemy to use SQLite database located at db.sqlite file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) # Create SQLAlchemy database instance
 
-
+# Define Todo model representing the 'todos' table in the database
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     completed = db.Column(db.Boolean, default=False)
 
 
+# Define route for the home page
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        title = request.form.get('title')
-        new_todo = Todo(title=title)
-        db.session.add(new_todo)
-        db.session.commit()
-        return redirect(url_for('home'))
-    else:
-        todo_list = Todo.query.all()
-        return render_template('base.html', todo_list=todo_list)
+    if request.method == 'POST':            # If form is submitted
+        title = request.form.get('title')   # Get the title of the new todo task from the form
+        new_todo = Todo(title=title)        # Create a new Todo object with the provided title
+        db.session.add(new_todo)            # Add the new todo task to the database session
+        db.session.commit()                 # Commit the changes to the database
+        return redirect(url_for('home'))    # Redirect to the home page to refresh the todo list
+    else:  # If the request is a GET request
+        todo_list = Todo.query.all()    # Query all todo tasks from the database
+        # Render the base.html template with the todo_list passed as context
+        return render_template('base.html', todo_list=todo_list) 
 
 
 @app.route('/edit/<int:id>', methods=['POST'])
@@ -42,8 +46,9 @@ def delete(id):
         db.session.commit()
         return redirect(url_for('home'))
     else:
-        # Handle other HTTP methods gracefully
+        # 
         return redirect(url_for('home'))
+
 
 @app.route('/toggle_complete/<int:id>', methods=['POST'])
 def toggle_complete(id):
